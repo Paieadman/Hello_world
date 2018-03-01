e56b645c043091810a147583bcd5123767384a39
Paieadman <36691797+Paieadman@users.noreply.github.com>
2018-03-01 12:32:13 +0300

    HelloWorld.scala

---
+++ HelloWorld.scala
@@ -0,0 +1,14 @@
+
+
+object HelloWorld {
+  def main(args:Array[String]){
+      println("Hello World")
+      hello()
+      val x =new hellooworld 
+      x.hello()
+  }
+  def hello() = println("hello World")
+class hellooworld{
+  def hello() = println("hello_world")
+}
+}
